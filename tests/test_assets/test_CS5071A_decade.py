d4ea7c397e1ccf5ee46dc2099a342b0a9f2b3ac7
Alvise <alvise.vianello13@gmail.com>
2020-12-12 09:55:04 +0100

    improved handling of assets directory in pytests

--- tests/test_assets/test_CS5071A_decade.py
+++ tests/test_assets/test_CS5071A_decade.py
@@ -19,112 +19,109 @@ import pytest
 import pathlib
 import allantoolkit
 import allantoolkit.testutils as testutils
-from typing import Callable
 
-data_file = '../assets/Cs5071A/5071A_phase.txt.gz'  # input data file
-verbose = 1
-tolerance = 1e-4
-rate = 1.  # stable32 runs were done with this data-interval
+# top level directory with asset files
+ASSETS_DIR = pathlib.Path(__file__).parent.parent / 'assets'
 
-this_dir = pathlib.Path(__file__).parent
+# input data files, and associated verbosity, tolerance, and acquisition rate
+assets = [
+    ('Cs5071A/5071A_phase.txt.gz', 1, 1e-4, 1.),
+]
 
-params = [
-    (data_file, 'adev_decade.txt', allantoolkit.allantools.adev),
-    (data_file, 'oadev_decade.txt', allantoolkit.allantools.oadev),
-    (data_file, 'mdev_decade.txt', allantoolkit.allantools.mdev),
-    (data_file, 'tdev_decade.txt', allantoolkit.allantools.tdev),
-    (data_file, 'hdev_decade.txt', allantoolkit.allantools.hdev),
-    (data_file, 'ohdev_decade.txt', allantoolkit.allantools.ohdev),
-    (data_file, 'totdev_decade.txt', allantoolkit.allantools.totdev),
-    (data_file, 'tierms_decade.txt', allantoolkit.allantools.tierms),
+# input result files and function which should replicate them
+results = [
+    ('adev_decade.txt', allantoolkit.allantools.adev),
+    ('oadev_decade.txt', allantoolkit.allantools.oadev),
+    ('mdev_decade.txt', allantoolkit.allantools.mdev),
+    ('tdev_decade.txt', allantoolkit.allantools.tdev),
+    ('hdev_decade.txt', allantoolkit.allantools.hdev),
+    ('ohdev_decade.txt', allantoolkit.allantools.ohdev),
+    ('totdev_decade.txt', allantoolkit.allantools.totdev),
+    ('tierms_decade.txt', allantoolkit.allantools.tierms),
 ]
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('datafile, result, fct', params)
-def test_generic(datafile: str, result: str, fct: Callable):
+@pytest.mark.parametrize('datafile, verbose, tolerance, rate', assets)
+@pytest.mark.parametrize('result, fct', results)
+def test_generic(datafile, result, fct, verbose, tolerance, rate):
 
-    datafile = pathlib.Path(datafile)
+    datafile = ASSETS_DIR / datafile
     result = datafile.parent / result
 
-    with testutils.working_directory(this_dir):
-        testutils.test_row_by_row(
-            fct, datafile, rate, result,
-            verbose=verbose, tolerance=tolerance)
+    testutils.test_row_by_row(fct, datafile, rate, result,
+                              verbose=verbose, tolerance=tolerance)
 
 
-params_ci = [
-    (data_file, 'adev_decade.txt', allantoolkit.allantools.adev, 2,
+results_ci = [
+    ('adev_decade.txt', allantoolkit.allantools.adev, 2,
      allantoolkit.ci.edf_greenhall, False, False),
-    (data_file, 'oadev_decade.txt', allantoolkit.allantools.oadev, 2,
+    ('oadev_decade.txt', allantoolkit.allantools.oadev, 2,
      allantoolkit.ci.edf_greenhall, True, False),
-    (data_file, 'mdev_decade.txt', allantoolkit.allantools.mdev, 2,
+    ('mdev_decade.txt', allantoolkit.allantools.mdev, 2,
      allantoolkit.ci.edf_greenhall, True, True),
-    (data_file, 'hdev_decade.txt', allantoolkit.allantools.hdev, 3,
+    ('hdev_decade.txt', allantoolkit.allantools.hdev, 3,
      allantoolkit.ci.edf_greenhall, False, False),
-    (data_file, 'ohdev_decade.txt', allantoolkit.allantools.ohdev, 3,
+    ('ohdev_decade.txt', allantoolkit.allantools.ohdev, 3,
      allantoolkit.ci.edf_greenhall, True, False),
 ]
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('datafile, result, fct, d, ci_fct, overlapping, '
-                         'modified', params_ci)
-def test_generic_ci(datafile: str, result: str, fct: Callable,
-                    d: int, ci_fct: Callable, overlapping: bool,
-                    modified: bool):
+@pytest.mark.parametrize('datafile, verbose, tolerance, rate', assets)
+@pytest.mark.parametrize('result, fct, d, ci_fct, overlapping, modified',
+                         results_ci)
+def test_generic_ci(datafile, result, fct, verbose, tolerance, rate,
+                    d, ci_fct, overlapping, modified):
 
-    datafile = pathlib.Path(datafile)
+    datafile = ASSETS_DIR / datafile
     result = datafile.parent / result
 
-    with testutils.working_directory(this_dir):
+    s32rows = testutils.read_stable32(resultfile=result, datarate=rate)
 
-        s32rows = testutils.read_stable32(resultfile=result, datarate=rate)
+    for row in s32rows:
 
-        for row in s32rows:
+        data = testutils.read_datafile(datafile)
 
-            data = testutils.read_datafile(datafile)
+        (taus, devs, errs, ns) = fct(data, rate=rate, taus=[row['tau']])
 
-            (taus, devs, errs, ns) = fct(data, rate=rate, taus=[row['tau']])
+        edf = ci_fct(
+            alpha=row['alpha'], d=d, m=row['m'], N=len(data),
+            overlapping=overlapping, modified=modified, verbose=True)
 
-            edf = ci_fct(
-                alpha=row['alpha'], d=d, m=row['m'], N=len(data),
-                overlapping=overlapping, modified = modified, verbose=True)
+        (lo, hi) = allantoolkit.ci.confidence_interval(
+            devs[0], ci=0.68268949213708585, edf=edf)
 
-            (lo, hi) = allantoolkit.ci.confidence_interval(
-                devs[0], ci=0.68268949213708585, edf=edf)
-
-            print("n check: ", testutils.check_equal( ns[0], row['n']))
-            print("dev check: ", testutils.check_approx_equal( devs[0], row['dev'] ) )
-            print("min dev check: ",  lo, row['dev_min'], testutils.check_approx_equal( lo, row['dev_min'], tolerance=1e-3 ) )
-            print("max dev check: ", hi, row['dev_max'], testutils.check_approx_equal( hi, row['dev_max'], tolerance=1e-3 ) )
+        print("n check: ", testutils.check_equal( ns[0], row['n']))
+        print("dev check: ", testutils.check_approx_equal( devs[0], row['dev'] ) )
+        print("min dev check: ",  lo, row['dev_min'], testutils.check_approx_equal( lo, row['dev_min'], tolerance=1e-3 ) )
+        print("max dev check: ", hi, row['dev_max'], testutils.check_approx_equal( hi, row['dev_max'], tolerance=1e-3 ) )
 
 
 #  Need custom test for totdev due to different edf signature
 @pytest.mark.slow
-def test_totdev_ci():
+@pytest.mark.parametrize('datafile, verbose, tolerance, rate', assets)
+def test_totdev_ci(datafile, verbose, tolerance, rate):
 
-    datafile = pathlib.Path(data_file)
+    datafile = ASSETS_DIR / datafile
     result = datafile.parent / 'totdev_decade.txt'
 
-    with testutils.working_directory(this_dir):
+    s32rows = testutils.read_stable32(resultfile=result, datarate=rate)
 
-        s32rows = testutils.read_stable32(resultfile=result, datarate=rate)
+    for row in s32rows:
 
-        for row in s32rows:
+        data = testutils.read_datafile(datafile)
 
-            data = testutils.read_datafile(datafile)
+        (taus, devs, errs, ns) = allantoolkit.allantools.totdev(
+            data, rate=rate, taus=[row['tau']])
 
-            (taus, devs, errs, ns) = allantoolkit.allantools.totdev(
-                data, rate=rate, taus=[row['tau']])
+        edf = allantoolkit.ci.edf_totdev(
+            alpha=row['alpha'], m=row['m'], N=len(data))
 
-            edf = allantoolkit.ci.edf_totdev(
-                alpha=row['alpha'], m=row['m'], N=len(data))
+        (lo, hi) = allantoolkit.ci.confidence_interval(
+            devs[0], ci=0.68268949213708585, edf=edf)
 
-            (lo, hi) = allantoolkit.ci.confidence_interval(
-                devs[0], ci=0.68268949213708585, edf=edf)
-
-            print("n check: ", testutils.check_equal( ns[0], row['n']))
-            print("dev check: ", testutils.check_approx_equal( devs[0], row['dev'] ) )
-            print("min dev check: ",  lo, row['dev_min'], testutils.check_approx_equal( lo, row['dev_min'], tolerance=1e-3 ) )
-            print("max dev check: ", hi, row['dev_max'], testutils.check_approx_equal( hi, row['dev_max'], tolerance=1e-3 ) )
+        print("n check: ", testutils.check_equal( ns[0], row['n']))
+        print("dev check: ", testutils.check_approx_equal( devs[0], row['dev'] ) )
+        print("min dev check: ",  lo, row['dev_min'], testutils.check_approx_equal( lo, row['dev_min'], tolerance=1e-3 ) )
+        print("max dev check: ", hi, row['dev_max'], testutils.check_approx_equal( hi, row['dev_max'], tolerance=1e-3 ) )

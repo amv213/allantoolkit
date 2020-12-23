1c2b51d985ec4401375c2d14069787b3bde4c1cd
Alvise <alvise.vianello13@gmail.com>
2020-12-23 14:33:00 +0100

    ported ocxo test to latest framework

--- tests/test_assets/test_ocxo.py
+++ tests/test_assets/test_ocxo.py
@@ -9,135 +9,49 @@
 
 """
 
+import logging
 import pathlib
 import pytest
 import allantoolkit
-import allantoolkit.testutils as testutils
-import numpy as np
 
 
-# top level directory with asset files
-ASSETS_DIR = pathlib.Path(__file__).parent.parent / 'assets'
+# Set testing logger to debug mode
+logging.basicConfig()
+logging.getLogger('allantoolkit.testutils').setLevel("DEBUG")
 
-# input data files, and associated verbosity, tolerance, and acquisition rate
-assets = [
-    ('ocxo/ocxo_frequency.txt', 1, 1e-4, 1.),
+# Directory with asset files
+ASSETS_DIR = pathlib.Path(__file__).parent.parent / 'assets/ocxo'
+
+# Raw data onto which to perform statistics and check it matches
+Y = allantoolkit.testutils.read_datafile(ASSETS_DIR / 'ocxo_frequency.txt')
+Y = allantoolkit.utils.frequency2fractional(f=Y, v0=1.0e7)
+
+# Data sampling rate
+RATE = 1.  # Hz
+
+# Function to check, test_alpha, test_ci
+# FIXME: Test_ci is set to False because CI implementation doesn't match yet
+#  Stable32 results.
+params = [
+    (allantoolkit.allantools.adev, True, False),
+    (allantoolkit.allantools.oadev, True, False),
+    (allantoolkit.allantools.mdev, True, False),
+    (allantoolkit.allantools.tdev, True, False),
+    (allantoolkit.allantools.hdev, True, False),
+    (allantoolkit.allantools.ohdev, True, False),
+    (allantoolkit.allantools.totdev, True, False),
 ]
 
 
-# input result files and function which should replicate them
-results_ci = [
-    ('adev_octave.txt', allantoolkit.allantools.adev, 2,
-     allantoolkit.ci.edf_greenhall, False, False),
-    ('oadev_octave.txt', allantoolkit.allantools.oadev, 2,
-     allantoolkit.ci.edf_greenhall, True, False),
-    ('mdev_octave.txt', allantoolkit.allantools.mdev, 2,
-     allantoolkit.ci.edf_greenhall, True, True),
-    ('tdev_octave.txt', allantoolkit.allantools.tdev, 2,
-     allantoolkit.ci.edf_greenhall, True, True),
-    ('hdev_octave.txt', allantoolkit.allantools.hdev, 3,
-     allantoolkit.ci.edf_greenhall, False, False),
-    ('ohdev_octave.txt', allantoolkit.allantools.ohdev, 3,
-     allantoolkit.ci.edf_greenhall, True, False),
-]
+@pytest.mark.parametrize('func, test_alpha, test_ci', params)
+def test_dev(func, test_alpha, test_ci):
+    """Test that Allantoolkit deviation results match the reference Stable32
+    results."""
 
+    fn = ASSETS_DIR / (func.__name__ + '_octave.txt')
 
-@pytest.mark.slow
-@pytest.mark.parametrize('datafile, verbose, tolerance, rate', assets)
-@pytest.mark.parametrize('result, fct, d, ci_fct, overlapping, modified',
-                         results_ci)
-def test_generic_ci(datafile, result, fct, verbose, tolerance, rate,
-                    d, ci_fct, overlapping, modified):
-
-    datafile = ASSETS_DIR / datafile
-    result = datafile.parent / result
-
-    s32rows = testutils.read_stable32(result)
-
-    for row in s32rows:
-        data = testutils.read_datafile(datafile)
-        data = allantoolkit.utils.frequency2fractional(data, v0=1.0e7)
-        (taus, devs, err_lo, errs_hi, ns) = fct(data,
-                                                rate=rate,
-                                                data_type="freq",
-                                                taus=row[1])
-
-        # NOTE! Here we use alhpa from Stable32-results for the allantools edf computation!
-        edf = ci_fct(alpha=row[3], d=d, m=row[0], N=len(data),
-                     overlapping=overlapping, modified=modified, verbose=True)
-
-        (lo, hi) = allantoolkit.ci.confidence_interval(devs[0], edf=edf)
-
-        print("n check: ", testutils.check_equal(ns[0], row[2]))
-        print("dev check: ", devs[0], row[5],
-              testutils.check_approx_equal(devs[0], row[5],
-                                           tolerance=2e-3))
-        print("min dev check: ", lo, row[4],
-              testutils.check_approx_equal(lo, row[4], tolerance=2e-3))
-        print("max dev check: ", hi, row[6],
-              testutils.check_approx_equal(hi, row[6], tolerance=5e-3))
-
-
-#  Need custom test for totdev due to different edf signature
-# fails, totdev() needs bias-correction, depending on alpha(?)
-@pytest.mark.skip(reason="needs bias-correction and noise-ID to work")
-@pytest.mark.xfail
-@pytest.mark.parametrize('datafile, verbose, tolerance, rate', assets)
-def test_totdev_ci(datafile, verbose, tolerance, rate):
-
-    datafile = ASSETS_DIR / datafile
-    result = datafile.parent / 'totdev_octave.txt'
-
-    s32rows = testutils.read_stable32(result)
-
-    for row in s32rows:
-        data = testutils.read_datafile(datafile)
-        data = allantoolkit.utils.frequency2fractional(data, v0=1.0e7)
-        (taus, devs, errs, ns) = allantoolkit.allantools.totdev(
-            data, rate=rate, data_type="freq", taus=[row['tau']])
-        edf = allantoolkit.ci.edf_totdev(
-            N=len(data), m=row['m'], alpha=row['alpha'])
-
-        (lo, hi) = allantoolkit.ci.confidence_interval(devs[0], edf=edf)
-
-        print("n check: ", testutils.check_equal(ns[0], row['n']))
-        print("dev check: ", testutils.check_approx_equal(devs[0], row['dev'],
-                                                          tolerance=2e-3))
-        print("min dev check: %.4g %.4g %d" % (
-            lo, row['dev_min'], testutils.check_approx_equal(
-                lo, row['dev_min'], tolerance=2e-3)))
-        print("max dev check: %.4g %.4g %d" % (
-            hi, row['dev_max'], testutils.check_approx_equal(
-                hi, row['dev_max'], tolerance=2e-3)))
-
-
-@pytest.mark.slow
-@pytest.mark.parametrize('datafile, verbose, tolerance, rate', assets)
-def test_noise_id(datafile, verbose, tolerance, rate):
-    """ test for noise-identification """
-
-    datafile = ASSETS_DIR / datafile
-    result = datafile.parent / 'mdev_octave.txt'
-
-    s32_rows = testutils.read_stable32(result)
-
-    freq = testutils.read_datafile(datafile)
-    phase = allantoolkit.utils.frequency2phase(freq, rate)
-
-    for s32 in s32_rows:
-        s32_tau, s32_alpha, s32_AF = s32[1], s32[3], int(s32[0])
-
-        # noise-ID from frequency
-        if len(phase) / s32_AF > 20:
-            alpha_int, alpha, d, rho = allantoolkit.ci.autocorr_noise_id(
-                freq, data_type='freq', af=s32_AF)
-
-            print("y: ", s32_tau, s32_alpha, alpha_int, alpha, rho, d)
-            assert alpha_int == s32_alpha
-
-            # noise-ID from phase
-        if len(phase) / s32_AF > 20:
-            alpha_int, alpha, d, rho = allantoolkit.ci.autocorr_noise_id(
-                phase, data_type='phase', af=s32_AF)
-            print("x: ", s32_tau, s32_alpha, alpha_int, alpha, rho, d)
-            assert alpha_int == s32_alpha
+    allantoolkit.testutils.test_Stable32_run(data=Y, func=func, rate=RATE,
+                                             data_type='freq',
+                                             taus='octave',
+                                             fn=fn, test_alpha=test_alpha,
+                                             test_ci=test_ci)

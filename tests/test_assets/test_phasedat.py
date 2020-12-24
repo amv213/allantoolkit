dfff0e45c44446ae2654fd7e442a0b5197b974bb
Alvise <alvise.vianello13@gmail.com>
2020-12-24 18:11:48 +0100

    patched test_phasedat with updated Stable32 1.61 datasets

--- tests/test_assets/test_phasedat.py
+++ tests/test_assets/test_phasedat.py
@@ -17,8 +17,8 @@ import allantoolkit
 logging.basicConfig()
 logging.getLogger('allantoolkit.testutils').setLevel("DEBUG")
 
-# Directory with asset files
-ASSETS_DIR = pathlib.Path(__file__).parent.parent / 'assets/phasedat'
+# Directory with asset files (Testing results from Stable 1.61)
+ASSETS_DIR = pathlib.Path(__file__).parent.parent / 'assets/phasedat/'
 
 # Raw data onto which to perform statistics and check it matches
 X = allantoolkit.testutils.read_datafile(ASSETS_DIR / 'PHASE.DAT')
@@ -26,39 +26,38 @@ X = allantoolkit.testutils.read_datafile(ASSETS_DIR / 'PHASE.DAT')
 # Data sampling rate
 RATE = 1.  # Hz
 
-# Function to check, test_alpha, test_ci
-# FIXME: test_alpha is set to false because it seems like reference file was
-#  using fixed alpha = 0, instead of using auto noise ID. Test_ci is set to
-#  False because CI implementation doesn't match yet Stable32 results.
+# Function to check
 params = [
-    (allantoolkit.allantools.adev, False, False),
-    (allantoolkit.allantools.oadev, False, False),
-    (allantoolkit.allantools.mdev, False, False),
-    (allantoolkit.allantools.tdev, False, False),
-    (allantoolkit.allantools.hdev, False, False),
-    (allantoolkit.allantools.ohdev, False, False),
-    (allantoolkit.allantools.totdev, False, False),
-    pytest.param(allantoolkit.allantools.htotdev, False, False,
+    allantoolkit.allantools.adev,
+    allantoolkit.allantools.oadev,
+    allantoolkit.allantools.mdev,
+    allantoolkit.allantools.tdev,
+    allantoolkit.allantools.hdev,
+    allantoolkit.allantools.ohdev,
+    allantoolkit.allantools.totdev,
+    pytest.param(allantoolkit.allantools.mtotdev,
                  marks=pytest.mark.slow),
-    pytest.param(allantoolkit.allantools.mtotdev, False, False,
+    pytest.param(allantoolkit.allantools.ttotdev,
                  marks=pytest.mark.slow),
-    pytest.param(allantoolkit.allantools.ttotdev, False, False,
+    pytest.param(allantoolkit.allantools.htotdev,
                  marks=pytest.mark.slow),
-    (allantoolkit.allantools.theo1, False, False),
-    (allantoolkit.allantools.mtie, False, False),
-    (allantoolkit.allantools.tierms, False, False),
+    allantoolkit.allantools.tierms,
+    allantoolkit.allantools.mtie,
+    allantoolkit.allantools.theo1,
 ]
 
 
-@pytest.mark.parametrize('func, test_alpha, test_ci', params)
-def test_dev(func, test_alpha, test_ci):
+# FIXME: Test_ci is set to False because CI implementation doesn't match yet
+#  Stable32 results.
+@pytest.mark.parametrize('func', params)
+def test_dev(func):
     """Test that Allantoolkit deviation results match the reference Stable32
     results."""
 
-    fn = ASSETS_DIR / ('phase_dat_' + func.__name__ + '_octave.txt')
+    fn = ASSETS_DIR / (func.__name__ + '_octave.txt')
 
     allantoolkit.testutils.test_Stable32_run(data=X, func=func, rate=RATE,
                                              data_type='phase',
                                              taus='octave',
-                                             fn=fn, test_alpha=test_alpha,
-                                             test_ci=test_ci)
+                                             fn=fn, test_alpha=True,
+                                             test_ci=False)
